76516c05840991e0588ad368dce1f04ef4c6cd48
jostomasqueirolo <jtqueirolo@miuandes.cl>
2021-05-05 20:02:24 -0400

    Funcion update creada

--- Lab4.js
+++ Lab4.js
@@ -1,4 +1,16 @@
 const vew = counter => `Counter: ${counter}`;
 
+const update = (msg,counter) => {
+    if(msg==="+"){
+        return counter+1
+    }
+    if(msg==="-"){
+        return counter-1
+    }
+    else{
+        return counter
+    }
 
+}
 console.log(vew(3));
+console.log(update("s", 5))
