b2cf290280a214a4877e44b58b8f0672bf0cad34
jostomasqueirolo <jtqueirolo@miuandes.cl>
2021-05-05 20:41:10 -0400

    Elimino archivo asdasd.js

--- asdasd.js
+++
@@ -1,8 +0,0 @@
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-  })
-  
-  readline.question(`What would you do? `, msg => {
-    readline.close()
-  })
